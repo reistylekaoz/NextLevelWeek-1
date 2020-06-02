f5d11ff30193138d0b18a88c96d362e2beddc0df
Murilo Vieira <murilovieiracardoso@gmail.com>
2020-06-02 01:29:25 -0300

    Criação do App

---
+++ backend/src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/users', (request, response)=> {
+    console.log('Listagem de Usuários');
+    response.send('Hello World');
+});
+
+app.listen(3333);
